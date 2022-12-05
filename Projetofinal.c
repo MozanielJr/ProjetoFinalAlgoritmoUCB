feaf7f3690b333544a6c137861c05fbf42fadcfc
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2022-12-04 23:48:01 -0200

    inteirasemaias

--- Projetofinal.c
+++ Projetofinal.c
@@ -10,9 +10,12 @@ int main()
     printf("Nome: Mozaniel Medeiros dos Santos Junior\nMatrícula: UC22103112\nCurso: Ciência da computação\n\n");
     printf("Nome: Nathalia Gonçalves Silva\nMatrícula: UC22200563\nCurso: Ciência da computação\n\n\n");
 
-    int sessions, peopleSessionOne, peopleSessionTwo, accountant=1, age, ticket, tickethalf;
-    char sex;
-
+    int sessions, peopleSessionOne, peopleSessionTwo, accountant=1, age, ticket, ticketHalf;
+    char sex, ingresso;
+	
+	ticket = 0;
+	ticketHalf = 0;
+	
     do 
     {
         printf("Digite o número de sessões que deseja (mínimo 2 e no máximo 2)\n");
@@ -44,12 +47,30 @@ int main()
                 	scanf("%d", &age);                	
 				}
                 	while (age < 3 || age > 100);
-                	
+                	fflush(stdin);
+                	do 
+				{
+					printf("\nUse --> I - Inteira, M - Meia-entrada\n\n Digite se é Inteira ou meia do telespectador %d: ", accountant);
+                	scanf("%c", &ingresso);
+            	}
+					while (ingresso != 'I' && ingresso != 'i' && ingresso != 'M' && ingresso != 'm');
+					switch (ingresso){
+						case 'M': ticketHalf = ticketHalf + 25; break;
+						case 'm': ticketHalf = ticketHalf + 25; break;
+						case 'I': ticket = ticket + 50; break;
+						case 'i': ticket = ticket + 50; break;
+			
+						
+						
+						
+						
+					}
                 accountant++;
             }
         
             while (accountant <= peopleSessionOne);
-       
+   
+   printf("Valor das Inteiras %d e Valor das Meias %d", ticket, ticketHalf);    
            accountant = 1;         
         printf("Informe a quantidade de pessoas na sessao 2\n");
         scanf("%d", &peopleSessionTwo);
@@ -58,7 +79,7 @@ int main()
         {
             printf("Use --> M - masculino, F - Feminino.\n\nDigite o sexo do telespectador %d: ", accountant);
             scanf("%s", &sex);
-        
+      
             printf("\nDigite a idade do telespectador %d: ", accountant);
             scanf("%d", &age);
                 if ( 3 < sex < 100);
